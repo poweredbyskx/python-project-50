df24c8371205fe122bb030402a0a5c7535515c3c
poweredbyskx <poweredbyskx@gmail.com>
2024-11-11 22:24:02 +0500

    fix test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -20,5 +20,4 @@ def get_path(path):
 def test_generate_diff(file1, file2, expected, format_output):
     with open(get_path(expected), 'r') as f:
         expected_result = f.read()
-    assert generate_diff(get_path(file1), get_path(file2),
-                         format_output) == expected_result
+    assert generate_diff(get_path(file1), get_path(file2), format_output).strip() == expected_result.strip()
